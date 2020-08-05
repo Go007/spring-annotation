86fda4941208581f49c50fd36ae49b6890e5f875
hong <1536054473@qq.com>
2020-08-05 21:37:57 +0800

    @PropertySource + @Value test

--- src/main/java/com/hong/bean/Person.java
+++ src/main/java/com/hong/bean/Person.java
@@ -1,5 +1,7 @@
 package com.hong.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Person {
 
 	public Person() {}
@@ -10,10 +12,15 @@ public class Person {
 		this.age = age;
 	}
 
+	//使用@Value赋值；
+	//1、基本数值
+	//2、可以写SpEL； #{}
+	//3、可以写${}；取出配置文件【properties】中的值（在运行环境变量里面的值）
+	@Value("张三")
 	private String name;
-
+	@Value("#{20-2}")
 	private Integer age;
-
+	@Value("${person.nickName}")
 	private String nickName;
 
 	public String getNickName() {
